2b6d0cdea968f58a12b055029b82f4d48d30706d
fracisconeto <vargasfrancisco227@gmail.com>
2025-10-29 19:59:06 -0300

    velas todas cadastradas

--- core/models/produto.py
+++ core/models/produto.py
@@ -1,31 +1,36 @@
-from email.mime import image
 from django.db import models
 from .categoria import Categoria
 from uploader.models import Image
-
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
     descricao = models.TextField(blank=True, null=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name="produtos")
+    categoria = models.ForeignKey(
+    Categoria,
+    on_delete=models.SET_NULL,
+    related_name="produtos",
+    null=True,
+    blank=True,
+    default=None,
+    )
+
     image = models.ForeignKey(
         Image,
-        related_name='+',
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
         default=None,
-        )
+        related_name='+',
+    )
     image2 = models.ForeignKey(
         Image,
-        related_name='+',
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
         default=None,
-        )
-  
+        related_name='+',
+    )
 
     def __str__(self):
         return self.nome
